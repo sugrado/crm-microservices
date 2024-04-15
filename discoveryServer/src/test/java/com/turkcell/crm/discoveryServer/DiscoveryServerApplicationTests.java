53d4de05b9b36d3ddbdbc0eef6c7e542e2213f88
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2024-04-15 21:01:48 +0300

    Initial commit

---
+++ discoveryServer/src/test/java/com/turkcell/crm/discoveryServer/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.crm.discoveryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
