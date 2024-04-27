75d2c076b75845b13ac99baf0209ec3f7613a364
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2024-04-27 19:52:40 +0300

    refactor: constant message namings and make created date as not nullable

--- customerService/src/main/java/com/turkcell/crm/customer_service/business/constants/messages/Messages.java
+++ customerService/src/main/java/com/turkcell/crm/customer_service/business/constants/messages/Messages.java
@@ -2,17 +2,17 @@ package com.turkcell.crm.customer_service.business.constants.messages;
 
 public class Messages {
     public static class CustomerMessages {
-        public static final String CUSTOMER_NOT_FOUND = "customerNotFound";
+        public static final String NOT_FOUND = "customerNotFound";
         public static final String EMAIL_ALREADY_EXISTS = "emailAlreadyExists";
     }
 
     public static class IndividualCustomerMessages {
         public static final String NOT_FOUND = "customerNotFound";
-        public static final String CUSTOMER_NATIONALITY_ID_ALREADY_EXISTS = "customerNationalityIdAlreadyExists";
-        public static final String CUSTOMER_NATIONALITY_ID_NOT_VALID = "customerNationalityIdNotValid";
+        public static final String NATIONALITY_ID_ALREADY_EXISTS = "customerNationalityIdAlreadyExists";
+        public static final String NATIONALITY_ID_NOT_VALID = "customerNationalityIdNotValid";
     }
 
-    public static class CityMessages{
+    public static class CityMessages {
         public static final String NOT_FOUND = "cityNotFound";
     }
 
