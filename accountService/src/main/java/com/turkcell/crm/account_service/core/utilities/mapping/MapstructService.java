1501656ddc66f94947c5519ffb764c6875e6b9f8
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2024-05-29 01:05:55 +0300

    refactor: add feign config to common package and change structure

--- accountService/src/main/java/com/turkcell/crm/account_service/core/utilities/mapping/MapstructService.java
+++
@@ -1,8 +0,0 @@
-package com.turkcell.crm.account_service.core.utilities.mapping;
-
-import org.mapstruct.MapperConfig;
-import org.mapstruct.ReportingPolicy;
-
-@MapperConfig(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
-public interface MapstructService {
-}
