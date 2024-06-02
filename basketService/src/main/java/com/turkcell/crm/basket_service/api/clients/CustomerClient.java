af4550903af70f11b1a445adf06eb903c651eb5b
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2024-06-02 16:32:21 +0300

    refactor: change service names to kebab-case

--- basketService/src/main/java/com/turkcell/crm/basket_service/api/clients/CustomerClient.java
+++ basketService/src/main/java/com/turkcell/crm/basket_service/api/clients/CustomerClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customerService", path = "customer-service/api/v1")
+@FeignClient(name = "customer-service", path = "customer-service/api/v1")
 public interface CustomerClient {
     @GetMapping("/customers/check-if-customer-exists/{customerId}")
     void checkIfCustomerExists(@PathVariable int customerId);
--- orderService/src/main/java/com/turkcell/crm/order_service/api/clients/CustomerClient.java
+++ orderService/src/main/java/com/turkcell/crm/order_service/api/clients/CustomerClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customerService", path = "customer-service/api/v1")
+@FeignClient(name = "customer-service", path = "customer-service/api/v1")
 public interface CustomerClient {
     @GetMapping("/individual-customers/invoice-info-by-address/{addressId}")
     GetIndividualCustomerInvoiceInfoDto getInvoiceInfoByAddress(@PathVariable int addressId);
